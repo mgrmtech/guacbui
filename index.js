057c27f0f365d6b7cc2b3671a22fbc43826c8dde
Sai Kishore Komanduri <saikishore.bits@gmail.com>
2020-01-27 11:50:40 +0530

    Remove CLI (split, and got into its own repo)

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const {importer, rollback} = require('./src/importer.js');
 const {convertXlsxToObj} = require('./src/parser.js');
 
-const {runCli} = require('./cli');
-
-runCli(importer, rollback, convertXlsxToObj);
+module.exports = {importer, rollback, convertXlsxToObj};
